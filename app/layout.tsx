9f524ed2586e3acca0e1f4a85e465ec44b92c2f1
Ron Kantor <ronkamail@gmail.com>
2024-02-21 21:16:20 +0200

    add web events

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { DirectionProvider } from "@radix-ui/react-direction";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -31,6 +32,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
