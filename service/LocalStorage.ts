339ed2f284302b448180b0f692d9ca5b8a245c62
Ron Kantor <ronkamail@gmail.com>
2024-02-23 20:17:32 +0200

    unused code cleanup

--- service/LocalStorage.ts
+++ service/LocalStorage.ts
@@ -2,20 +2,19 @@
 
 // Function to get data from local storage
 export function getDataFromLocalStorage<T>(key: string): T | null {
-	try {
-	  const item = localStorage.getItem(key);
-	  return item ? JSON.parse(item) : null;
-	} catch (error) {
-	  console.error('Error getting data from local storage:', error);
-	  return null;
-	}
+  try {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
+  } catch (error) {
+    console.error("Error getting data from local storage:", error);
+    return null;
   }
-  
-  export function setDataInLocalStorage<T>(key: string, value: T): void {
-	try {
-	  localStorage.setItem(key, JSON.stringify(value));
-	} catch (error) {
-	  console.error('Error setting data in local storage:', error);
-	}
+}
+
+export function setDataInLocalStorage<T>(key: string, value: T): void {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error("Error setting data in local storage:", error);
   }
-  
+}
