172bb34d0227ae31d81540506c5928e7debc6d9b
Ron Kantor <ronkamail@gmail.com>
2024-03-03 10:16:46 +0200

    add gender type

--- types/TinderCard.ts
+++ types/TinderCard.ts
@@ -1,16 +1,22 @@
 import z from "zod";
 
+const GenderSchema = z.union([
+  z.literal("m"),
+  z.literal("f"),
+  z.literal("mixed"),
+]);
+
+export type GenderType = z.infer<typeof GenderSchema>;
+
 // types.ts
 export interface TinderItem {
-	title: string;
-	emoji: string;
-	gender: string;
+  title: string;
+  emoji: string;
+  gender: GenderType;
 }
 
 export const TinderItemSchema = z.object({
-	title: z.string(),
-	emoji: z.string(),
-	gender: z.string(),
-  });
-  
-  
+  title: z.string(),
+  emoji: z.string(),
+  gender: z.string(),
+});
